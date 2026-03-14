27089cc6a9f80cac4b286cfe625ee498f4808eec
Kareem Naous <62202089+KareemN1936@users.noreply.github.com>
2026-03-14 22:18:12 +0200

    Made login page  check the account that was created in the Register page and connected "get started" button to register page.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,54 +1,72 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+	const navigate = useNavigate()
 
-        if (!email || !password) {
-            alert("Please fill in all fields");
-            return;
-        }
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
 
-        alert("Login successful");
-    } ;
+	const handleSubmit = (e) => {
+		e.preventDefault()
 
-  return (
-    <div style={{ maxWidth: "400px", margin: "50px auto" }}>
-        <h1>Login</h1>
+		if (!email || !password) {
+			alert("Please fill in all fields")
+			return
+		}
 
-        <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-        />
+		const savedUser = JSON.parse(localStorage.getItem("user"))
 
-        <br /><br />
+		if (!savedUser) {
+			alert("No account found. Please register first.")
+			return
+		}
 
-        <label>Password</label>
-        <input
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
+		if (email === savedUser.email && password === savedUser.password) {
+			alert("Login successful")
+			navigate("/dashboard")
+		} else {
+			alert("Invalid email or password")
+		}
+	}
 
-        <br /><br />
+	return (
+		<div style={{ maxWidth: "400px", margin: "50px auto" }}>
+			<h1>Login</h1>
 
-        <button type="submit">Login</button>
-        </form>
-      
-        <p>
-            Don&apos;t have an account? <Link to="/register">Register</Link>
-        </p>
-    </div>
-  );
+			<form onSubmit={handleSubmit}>
+
+				<label>Email</label>
+				<input
+					type="email"
+					placeholder="Enter email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+
+				<br /><br />
+
+				<label>Password</label>
+				<input
+					type="password"
+					placeholder="Enter password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+
+				<br /><br />
+
+				<button type="submit">Login</button>
+
+			</form>
+
+			<p>
+				Don&apos;t have an account? <Link to="/register">Register</Link>
+			</p>
+
+		</div>
+	)
 }
 
-export default Login;
+export default Login
